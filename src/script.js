deb426ab8798998232dd13e8d48d21c013fd6719
frykhamn <frykhamnz@gmail.com>
2025-02-10 14:43:18 +0100

    done

--- src/script.js
+++ src/script.js
@@ -1,23 +1,44 @@
 // Skapa objektet contactBook
-let contactBook;
-  
-  // Ange en grupp
-  let groupToShow;
-  
+const contactBook = {
+  owner: "Anna López",
+  contacts: [
+    {
+      name: "Ahmed Ali",
+      phone: "070-1234567",
+      group: "arbete",
+    },
+    {
+      name: "Chen Li",
+      phone: "070-7654321",
+      group: "vänner",
+    },
+    {
+      name: "Isak Gran",
+      phone: "070-1112223",
+      group: "arbete",
+    },
+    {
+      name: "Juan López",
+      phone: "070-5556667",
+      group: "familj",
+    },
+  ],
+};
+
+// Be användaren ange en grupp
+const groupToShow = "familj"
 
 
 
-function showContacts(contactBook, groupToShow) { // Rör ej denna kod
-
-
-
-    // Loopar igenom alla kontakter och jämför grupp
-
-console.log("hej")
-
+// Define and export functionName1
+function showContacts(contactBook, groupToShow) {
+  // Loopar igenom alla kontakter och jämför grupp
+  for (let contact of contactBook.contacts) {
+      if (contact.group === groupToShow) {
+          return `Namn: ${contact.name}, Telefon: ${contact.phone}`;
+      }
+  }
 }
 
-
-// Rör ej denna kod
 showContacts(contactBook, groupToShow);
 module.exports = { showContacts };
